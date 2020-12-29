1549526d64a5c6eeb8d44dcade9abcf2e870e41b
Ruslan <rusvolk97i@gmail.com>
2020-12-29 22:57:37 +0300

    beta(0.5) todolist

--- src/js/index.js
+++ src/js/index.js
@@ -3,6 +3,12 @@
 const input = document.querySelector('.input');
 const ul = document.querySelector('.ul');
 const control = document.querySelector('.control-panel');
+const btnComplete = document.querySelector('.complete');
+const btnActive = document.querySelector('.active');
+const btnAll = document.querySelector('.all');
+const btnClearCompleted = document.querySelector('.clear');
+
+
 let storage = {};
 let max = 0;
 
@@ -40,7 +46,9 @@ function buildUl(event){
 
   localStorage.setItem(`${key}`, event.target.value);
 
+  initializeChanging();
   event.target.value = '';
+  checkboxStroke();
 }
 
 function deleteFromList(element){
@@ -53,7 +61,9 @@ function deleteFromList(element){
   });
   cros.forEach((item) =>{
     item.addEventListener('click', (event) =>{
-      removeFromStorage(event.target.previousElementSibling.dataset.local);
+      console.log();
+      saveOrDelCheked(event.target.previousElementSibling, false, false);
+      removeFromStorage(event.target.parentElement.nextElementSibling.dataset.local);
       converseToStorage();
       event.target.parentElement.parentElement.remove();
       counter();
@@ -61,31 +71,51 @@ function deleteFromList(element){
   });
 }
 
+
+
+
 function checkboxStroke(){
   const checkbox = document.querySelectorAll('.checkbox');
   checkbox.forEach(item =>{
+    ifCheked(item);
     item.addEventListener('change', (e) =>{
       if(e.target.checked){
         e.target.parentElement.parentElement.classList.add('pop');
+        showClearCompleted();
         counter();
+        saveOrDelCheked(e.target, true, true);
       } else{
         e.target.parentElement.parentElement.classList.remove('pop');
+        showClearCompleted();
         counter();
+        saveOrDelCheked(e.target, true, false);
       }
       counter();
-    });
+    }, false);
   });
 }
+
 
 function counter(some){
   const counter = document.querySelector('.counter');
   const items = document.querySelectorAll('.tdl-item');
   const number = [];
   items.forEach(item =>{
-    if(!item.classList.contains('hide') && item.firstElementChild.firstElementChild.checked == false){
-        number.push(item);
-    } else if(some && item.classList.contains('pop')){
+    // if(!item.classList.contains('hide') && item.firstElementChild.firstElementChild.checked == false){
+    //   number.push(item);
+    // } else if(some && item.classList.contains('pop')){
+    //   number.push(item);
+    // } 
+    if(btnAll.classList.contains('active-btn')){
       number.push(item);
+    } else if(btnComplete.classList.contains('active-btn')){
+      if(item.classList.contains('pop')){
+        number.push(item);
+      }
+    } else if(btnActive.classList.contains('active-btn')){
+      if(!item.classList.contains('hide') && item.firstElementChild.firstElementChild.checked == false){
+          number.push(item);
+      } 
     }
   });
   const counts = document.createElement('span');
@@ -103,7 +133,7 @@ function counter(some){
 
 function recount(){
   let res = [];
-    for(let [key ,index] of Object.entries(storage)){
+    for(let [key, index] of Object.entries(storage)){
       res.push(index);
       for(let i = 0; i < res.length; i++){
         if(i != (res.length - 1)){
@@ -123,14 +153,15 @@ function printList(key, text){
   element.innerHTML = `
   <label>
     <input class="checkbox" type="checkbox">
-    <span data-local='${key}' class="text">${text}</span>
     <img class="crist hide" src="./img/crist.png" alt="crist">
   </label>
+  <span data-local='${key}' class="text">${text}</span>
   `;
   ul.insertBefore(element, control);
   checkboxStroke(ul);
   deleteFromList(element);
   counter();
+  initializeChanging();
 }
 
 function removeFromStorage(key){
@@ -143,7 +174,49 @@ function converseToStorage(){
   localStorage.setItem(`Ukey`, convers);
 }
 
-const btnAll = document.querySelector('.all');
+function changeText(e){
+  const typus = document.createElement('input');
+  let tdl = e.target.parentElement;
+  typus.classList.add('changer');
+  typus.value = `${tdl.lastElementChild.textContent}`;
+  tdl.firstElementChild.style.display = 'none';
+  tdl.lastElementChild.style.display = 'none';
+  let span = tdl.lastElementChild.dataset.local;
+  let index = storage[tdl.lastElementChild.dataset.local];
+  tdl.append(typus);
+  closeChangeInput(typus, e.target.parentElement, e.target.parentElement);
+  typus.addEventListener('change', (e) =>{
+    tdl.children[1].textContent = `${typus.value}`;
+    storage[span] = index;
+    localStorage.setItem(span, typus.value);
+    converseToStorage();
+    tdl.firstElementChild.style.display = 'block';
+    tdl.children[1].style.display = 'block';
+    e.target.remove();
+    initializeChanging();
+  });
+}
+
+function closeChangeInput(closingItem, retrieved, returned){
+  document.addEventListener('keydown', (e) =>{
+    if(e.code === 'Escape' && closingItem){
+      closingItem.remove();
+      retrieved.firstElementChild.style.display = 'block';
+      returned.lastElementChild.style.display = 'block';
+      initializeChanging();
+    }
+  });
+
+  document.addEventListener('click', (event) => {
+    if(event.target != closingItem){
+      closingItem.remove();
+      retrieved.firstElementChild.style.display = 'block';
+      returned.lastElementChild.style.display = 'block';
+      initializeChanging();
+    }
+  });
+}
+
 btnAll.addEventListener('click', () => showAll());
 function showAll(){
   const localTdl = document.querySelectorAll('.tdl-item');
@@ -153,7 +226,6 @@ function showAll(){
   });
 }
 
-const btnActive = document.querySelector('.active');
 btnActive.addEventListener('click', () =>{
   const localTdl = document.querySelectorAll('.tdl-item');
   localTdl.forEach(item =>{
@@ -167,7 +239,6 @@ btnActive.addEventListener('click', () =>{
   });
 });
 
-const btnComplete = document.querySelector('.complete');
 btnComplete.addEventListener('click', () =>{
   const localTdl = document.querySelectorAll('.tdl-item');
   localTdl.forEach(item =>{
@@ -181,12 +252,22 @@ btnComplete.addEventListener('click', () =>{
   });
 });
 
-const btnClearCompleted = document.querySelector('.clear');
+function showClearCompleted(){
+  const tdItem = document.querySelector('.tdl-item.pop');
+  if(tdItem){
+    btnClearCompleted.classList.remove('hide');
+  } else {
+    btnClearCompleted.classList.add('hide');
+  }
+}
+
+
 btnClearCompleted.addEventListener('click', () =>{
   const localItems = document.querySelectorAll('.tdl-item');
   localItems.forEach(item =>{
     if(item.classList.contains('pop')){
-      removeFromStorage(item.firstElementChild.children[1].dataset.local);
+      saveOrDelCheked(item.firstElementChild.firstElementChild, true, false);
+      removeFromStorage(item.lastElementChild.dataset.local);
       converseToStorage();
       item.remove();
       counter();
@@ -200,12 +281,60 @@ arrow.addEventListener('click', () =>{
   all.forEach(item =>{
   if(item.checked == true){
     item.checked = false; 
-    item.parentElement.classList.remove('pop');
+    item.parentElement.parentElement.classList.remove('pop');
     counter();
   } else{
     item.checked = true;
-    item.parentElement.classList.add('pop');
+    item.parentElement.parentElement.classList.add('pop');
     counter();
   }
   });
 });
+
+
+function initializeChanging(){
+  const todoI = document.querySelectorAll('.tdl-item .text');
+  todoI.forEach(item =>{
+    item.addEventListener('dblclick', changeText, {
+      once: true
+    });
+    item.addEventListener('selectstart', (event) =>{
+      event.preventDefault();
+    });
+  });
+}
+
+
+const controlBlok = document.querySelector('.control-block');
+
+const btns = controlBlok.querySelectorAll('button');
+btns.forEach(item => {
+  item.addEventListener('click', (e) =>{
+    btns.forEach(item => item.classList.remove('active-btn'));
+    e.target.classList.toggle('active-btn');
+    counter();
+  });
+});
+
+function saveOrDelCheked(target, boolean, switcher){
+  let parent = target.parentElement.parentElement;
+  let id = `${parent.lastElementChild.dataset.local}isChecked`;
+  if(switcher){
+    if(!localStorage.getItem(id)){
+      localStorage.setItem(id, boolean);
+    }
+  } else {
+    localStorage.removeItem(id);
+  }
+}
+
+function ifCheked(target){
+  let parent = target.parentElement.parentElement;
+  let id = `${parent.lastElementChild.dataset.local}isChecked`;
+  if(localStorage.getItem(id)){
+    target.checked = true;
+    target.parentElement.parentElement.classList.add('pop');
+    showClearCompleted();
+  }
+}
+
